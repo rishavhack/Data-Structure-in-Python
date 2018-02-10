c69db2fd0cee1ae0c1dd8cbf12c06c11ead1bf93
Rishav <rishav.jnit@gmail.com>
2018-02-10 19:11:43 +0530

    Python Introduction

---
+++ Introduction/string.py
@@ -0,0 +1,8 @@
+name = "Rishav"
+last = "Srivastava"
+year= 22
+
+sentence = name + ' ' + last + ' ' + str(year)
+
+print(sentence)
+print name,'is',last,year 
---
+++ Introduction/stringslice.py
@@ -0,0 +1,20 @@
+greeting="Hello world"
+print greeting
+print "greeting[0]",greeting[0]
+print "greeting with length",len(greeting)
+print "greeting[2]+greeting[5]",greeting[2],greeting[7]
+word="Python"
+print word
+print "word[2:5]",word[2:5]
+print "word[0:2]",word[0:2]
+print "word[:2]",word[:2]
+print "word[3:]",word[3:]
+print "greeting[5:] and word[:2]",greeting[5:],word[:2]
+word1="Goods"
+print len(word1)
+half1=len(word1)/2
+print "half1=len(word1)/2",half1
+print "Above return float number:indexes have to be integers-floats won't work"
+print "So we use //2"
+half1=len(word1)//2
+print "half1=len(word1)//2",half1
