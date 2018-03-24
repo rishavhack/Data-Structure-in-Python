a9878d28cc17cbed786aba858f243a2110f73d13
Rishav <rishav.jnit@gmail.com>
2018-03-24 14:37:53 +0530

    This program works in python 3

---
+++ Variable/end parameter in print.py
@@ -0,0 +1,10 @@
+#By default python’s print() function ends with a newline. 
+#This program work in python 3
+print("Welcome to" , end = ' ') 
+print("GeeksforGeeks", end = ' ')
+
+#OUtput-->Welcome to GeeksforGeeks
+
+print("Python" , end = '@') 
+print("GeeksforGeeks")
+#Output-->Python@GeeksforGeeks
