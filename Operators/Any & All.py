e4c01f3a54e624899b7a038f1116cf6185a3319a
Rishav <rishav.jnit@gmail.com>
2018-03-24 23:41:41 +0530

    All and Any in Python

---
+++ Operators/Any & All.py
@@ -0,0 +1,10 @@
+#Returns true if any of the items is True.
+print any([False,False,False,False])
+print any([False,True,False,True])
+print any([True,False,False,False])
+print '\n';
+
+#Returns true if all of the items are True (or if the iterable is empty). 
+print all[True,True,True,True]
+print all[False,True,True,False]
+print all[False,False,False]
