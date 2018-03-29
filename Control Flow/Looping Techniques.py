48bc88311c34533f3e63884ea70883610564aa16
Rishav <rishav.jnit@gmail.com>
2018-03-29 23:52:53 +0530

    Looping Techniques.py

--- Control Flow/Looping Techniques.py
+++ Control Flow/Looping Techniques.py
@@ -10,3 +10,23 @@ answers = ['apple', 'red', 'a circle']
 for question,answer in zip(questions,answers):
 	print "what is ur {{0}}? I am {{1}}".format(question,answer)
 
+d = { "geeks" : "for", "only" : "geeks" }
+ 
+# using iteritems to print the dictionary key-value pair
+print ("The key value pair using iteritems is : ")
+for i,j in d.iteritems():
+    print i,j
+
+
+ print "\n"
+
+ basket = ['guave', 'orange', 'apple', 'pear', 
+          'guava', 'banana', 'grape']
+ 
+# using sorted() and set() to print the list
+#  in sorted order
+for fruit in sortedset(basket):
+    print fruit
+
+
+print "\n"
