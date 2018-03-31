4334974e5867222cdbb5354151c2c54d28fc642e
Rishav <rishav.jnit@gmail.com>
2018-03-31 09:49:38 +0530

    *args use to pass list and **kwargs use to pass dictionary

--- Functions/args and kwargs.py
+++ Functions/args and kwargs.py
@@ -11,3 +11,14 @@ def hello(**kwargs):
 			print "%s==%s"%(key,value)
 
 hello(name="RishavIsName")
+
+def test(arg1,arg2,arg3):
+	print "arg1",arg1
+	print "arg2",arg2
+	print "arg3",arg3
+
+args=("hey",17,"Rishav")
+test(*args)
+print "\n"
+kwargs={"arg1":"hey","arg2":"14","arg3":"Rishav"}
+test(**kwargs)
