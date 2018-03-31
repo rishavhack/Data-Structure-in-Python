bcf6f9f9df01fad7c006dad40ad2f7ce5a505ba2
Rishav <rishav.jnit@gmail.com>
2018-03-31 09:35:53 +0530

    Floor details

---
+++ Functions/Precision Handling.py
@@ -0,0 +1,15 @@
+import math
+a = 3.4536
+
+print "The integer value  of number is"
+print math.trunc(a)
+print "\n"
+print math.ceil(a)
+print "\n"
+print math.floor(a)
+print "\n"
+print '%.2f'%a
+print "\n"
+print '{0:.2f}'.format(a)
+print "\n"
+print round(a,2)
