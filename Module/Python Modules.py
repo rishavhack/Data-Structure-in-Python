9a9197aec748ddddb24e95ba0bd1e9522412a789
Rishav <rishav.jnit@gmail.com>
2018-04-02 21:54:55 +0530

    Only on random number

--- Module/Python Modules.py
+++ Module/Python Modules.py
@@ -2,3 +2,24 @@ import math
 print math.sqrt(25)
 print math.pi
 print math.degrees(2)
+print math.sin(2) 
+print math.tan(0.23)
+ 
+# 1 * 2 * 3 * 4 = 24
+print math.factorial(4) 
+ import random
+ 
+# printing random integer between 0 and 5
+print random.randint(0, 5) 
+ 
+# print random floating point number between 0 and 1
+print random.random() 
+ 
+# random number between 0 and 100
+print random.random() * 100
+ 
+List = [1, 4, True, 800, "python", 27, "hello"]
+ 
+# using choice function in random module for choosing 
+# a random element from a set such as a list
+print random.choice(List)
