e3ca1661144d4713f5aa0d4eaecfd38445391378
Rishav <rishav.jnit@gmail.com>
2018-04-05 22:11:17 +0530

    Numeric Functions.py

---
+++ Module/Numeric Functions.py
@@ -0,0 +1,23 @@
+import math
+ 
+a = 2.3
+ 
+# returning the ceil of 2.3
+print ("The ceil of 2.3 is : ")
+print (math.ceil(a))
+ 
+# returning the floor of 2.3
+print ("The floor of 2.3 is : ")
+print (math.floor(a))
+
+a = -10
+ 
+b= 5
+ 
+# returning the absolute value.
+print ("The absolute value of -10 is : ")
+print (math.fabs(a))
+ 
+# returning the factorial of 5
+print ("The factorial of 5 is : ")
+print (math.factorial(b))
