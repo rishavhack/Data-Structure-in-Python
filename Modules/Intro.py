c4433857d30a52778ef5e5fb4b6420caaf9a5734
Rishav <rishav.jnit@gmail.com>
2018-04-10 22:10:16 +0530

    Work on Module

--- Modules/Intro.py
+++ Modules/Intro.py
@@ -41,3 +41,29 @@ print math.tan(0.23)
  
 # 1 * 2 * 3 * 4 = 24
 print math.factorial(4) 
+import random
+ 
+# printing random integer between 0 and 5
+print random.randint(0, 5) 
+ 
+# print random floating point number between 0 and 1
+print random.random() 
+ 
+# random number between 0 and 100
+print random.random() * 100
+ 
+List = [1, 4, True, 800, "python", 27, "hello"]
+ 
+# using choice function in random module for choosing 
+# a random element from a set such as a list
+print random.choice(List)
+ 
+ 
+# importing built in module datetime
+import datetime
+from datetime import date
+import time
+ 
+# Returns the number of seconds since the
+# Unix Epoch, January 1st 1970
+print time.time() 
