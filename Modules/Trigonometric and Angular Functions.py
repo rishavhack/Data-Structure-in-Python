3441579b10de90c29bab91d2811ed2aef04f464d
Rishav <rishav.jnit@gmail.com>
2018-04-11 13:20:55 +0530

    Trigo

---
+++ Modules/Trigonometric and Angular Functions.py
@@ -0,0 +1,10 @@
+import math
+a = math.pi/6
+print "math.pi/6",a
+print "The value of sine of pi/6 is :",math.sin(a)
+print "The value of cosine of pi/6 : ",math.cos(a)
+b=3
+c=4
+print "tanget of pi/6",math.tan(a)
+# hypot(a, b) :- This returns the hypotenuse of the values passed in arguments. Numerically, it returns the value of sqrt(a*a + b*b).
+print "The calue of hypotenuse of 3 and 4 :",math.hypot(b,c)
