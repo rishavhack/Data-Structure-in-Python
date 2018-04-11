8770e68b5525bd7d1bfd6db68fd0f45d0251c47f
Rishav <rishav.jnit@gmail.com>
2018-04-11 09:20:04 +0530

    Library of Math and lear ceil,floor except GCD

--- Modules/Intro.py
+++ Modules/Intro.py
@@ -1,7 +1,3 @@
-# importing  module calc.py
-import calc
- 
-print add(10, 2)
 # importing sqrt() and factorial from the 
 # module math
 from math import sqrt, factorial
@@ -10,7 +6,8 @@ from math import sqrt, factorial
 # math.sqrt(16) and math.factorial()
 # are required.
 print sqrt(16)
-print factorial(6)
+print factorial(5)
+import math
 #  Import built-in module  random
 import  random
 print  dir(math)
@@ -29,9 +26,9 @@ print math.degrees(2)
  
 # 60 degrees = 1.04 radians
 print math.radians(60) 
- 
+print "\n"
 # Sine of 2 radians
-print math.sin(2) 
+print math.sin(90) 
  
 # Cosine of 0.5 radians
 print math.cos(0.5) 
@@ -45,13 +42,13 @@ import random
  
 # printing random integer between 0 and 5
 print random.randint(0, 5) 
- 
+
 # print random floating point number between 0 and 1
 print random.random() 
- 
+print "\n"
 # random number between 0 and 100
 print random.random() * 100
- 
+
 List = [1, 4, True, 800, "python", 27, "hello"]
  
 # using choice function in random module for choosing 
