45d134948e8530418fa7d896ba74d7ba48543118
Rishav <rishav.jnit@gmail.com>
2018-04-11 13:16:04 +0530

    Logarithmic and Power Functions.py

---
+++ Modules/Logarithmic and Power Functions.py
@@ -0,0 +1,8 @@
+import math 
+print "The e**4 value is ",math.exp(4)
+#Log 2 with base 3
+print "The value of log 2 with base 3 is:",math.log(2,3)
+#Log10
+print "Teh value of log10 of 10000",math.log10(10000)
+print "\n"
+print "The value of 3 to the power 2 is",math.pow(3,2)
---
+++ Modules/Numeric Functions.py
@@ -0,0 +1,19 @@
+import math 
+a =2.3
+print "Ceil",math.ceil(a);
+print "Floor",math.floor(a);
+a=-10
+print a
+b=5
+print b
+print "Absolute value of a ",math.fabs(a)
+print "Factorial of b",math.factorial(b)
+print "\n"
+b=5.5
+c=15
+d=5
+import math
+print "value of a,b,c,d",a,b,c,d
+print "Copy Sign",math.copysign(5.5,-10)
+#This work in only python 3.0
+print "Gcd of 5 and 15 is :",math.gcd(5,15)
