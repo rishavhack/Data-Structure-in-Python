83ec3514af2dd78712884142682e417292ebd69c
Rishav <rishav.jnit@gmail.com>
2018-04-11 14:00:55 +0530

    Convert Text to Speech done with user input

--- Convert Text to Speech/ConvertTexttoSpeech.py
+++ Convert Text to Speech/ConvertTexttoSpeech.py
@@ -1,7 +1,9 @@
 #pip install gTTS  in python 27 folder in script
 from gtts import gTTS
 import os
-myText = "Welcome Rishav,How are you and whats going"
-language = 'en'
+language = 'hi'
+myText = raw_input()
+
 myobj = gTTS(text=myText,lang=language,slow=False);
 myobj.save("nameofFile.mp3")
+os.system("mpg321 nameofFile.mp3")
