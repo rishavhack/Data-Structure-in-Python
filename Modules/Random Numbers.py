13bba9492012cab052cd1c1093497a1a6e674e73
Rishav <rishav.jnit@gmail.com>
2018-04-16 13:16:26 +0530

    Random number initialization

---
+++ Modules/Random Numbers.py
@@ -0,0 +1,7 @@
+import random
+print "A random number form list "
+print random.choice([1,5,3,8,4])
+print random.randrange(20,50,3)
+print random.random()
+print random.seed(5)
+print random.uniform(5,10)
