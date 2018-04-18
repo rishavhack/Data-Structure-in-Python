b8db480bd3d63878435fdcc9a77f9402b862851e
Rishav <rishav.jnit@gmail.com>
2018-04-18 13:32:23 +0530

    Intro of string

---
+++ String/intrr.py
@@ -0,0 +1,17 @@
+a ="Rishav"
+# a[2]="R"
+#Its create error 
+print "a-> ",a
+print "a[2]='R' its create error "
+a = a + 'for'
+print "a = a+'for'=>",a
+
+a = "Rishav"
+b = "Srivastava"
+c = '''Hello 
+every one'''
+print "a->",a
+print "b->",b
+print "c->",c
+print "a + b + c =>",a+b+c
+print "He said \"Hello Rishav \" "
