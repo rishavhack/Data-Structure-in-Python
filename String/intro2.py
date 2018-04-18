d5662107a57d42f5dac549935e94abbcb22a27d9
Rishav <rishav.jnit@gmail.com>
2018-04-18 13:35:39 +0530

    Intro 2nd Part

---
+++ String/intro2.py
@@ -0,0 +1,7 @@
+x = "Hell everyone Rishav"
+print x
+print "x[2]->",x[2]
+print "x[-3]->",x[-3]
+print "x[20]-> Index error string index of range"
+print "x[2:5]->",x[2:5]
+print "x[-5:-3]->",x[-5:-3]
