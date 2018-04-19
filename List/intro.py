789dff1af6b7fc6ede07646c87a9b682726dd9ac
Rishav <rishav.jnit@gmail.com>
2018-04-19 13:12:52 +0530

    List

---
+++ List/intro.py
@@ -0,0 +1,5 @@
+l=[1,"a","string",1+2]
+print l
+l.append(6)
+print l
+print l[1]
---
+++ String/String Methods 1.py
@@ -0,0 +1,3 @@
+str1 = "geeksforgeeks is for geeks"
+str2 ="is"
+print "THe first Occurence of str2 is at :",str1.find(str2)
