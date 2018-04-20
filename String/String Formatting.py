37fc196671d679444cac4fae76121dd5d2acb3ab
Rishav <rishav.jnit@gmail.com>
2018-04-20 13:46:45 +0530

    String Format %d,%r,%h

---
+++ String/String Formatting.py
@@ -0,0 +1,8 @@
+variable = '15'
+print "variable = %s"%variable
+print "Variable as %r"%variable
+variable = int(variable)#Without this below code show error
+print "Varibale as interger %d"% (variable)
+print "Varibale float %f"%(variable)
+print "Variable Hexadecimal  =%x"%(variable)
+print "Variable Octal = %o"%(variable)
