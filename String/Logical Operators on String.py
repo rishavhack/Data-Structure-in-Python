dd946c34a5e4f5f2fcf65ac2b4693e0657aaec1b
Rishav <rishav.jnit@gmail.com>
2018-04-20 13:32:40 +0530

    Logical method in string through repr

---
+++ String/Logical Operators on String.py
@@ -0,0 +1,10 @@
+str1 = ""
+str2 = 'geeks'
+print repr(str1 and str2)
+print repr(str1 or str2)
+
+str1 ="for"
+print repr(str1 and str2)
+print repr(str2 and str1)
+print repr(str1 or str2)
+print repr(str2 or str1)
