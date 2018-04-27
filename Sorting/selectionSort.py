6912d9494292f13807febb3b55abf07af40d9274
Rishav <rishav.jnit@gmail.com>
2018-04-27 21:02:12 +0530

    Sorting

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,12 +1,12 @@
 def SelectionSort(l):
 	for start in range(len(l)):
 		minpos = start
-		print minpos
+		print "minpos",minpos
 		for i in range(start,len(l)):
 			if l[i] < l[minpos]:
-				print i,minpos
 				minpos=i
 				(l[start],l[minpos])=(l[minpos],l[start])
+				print l;
 	return l
 
 lst = range(5,2,-1)
