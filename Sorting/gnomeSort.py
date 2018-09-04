dd05698bf289b3b830dfaaf8c2d7ba99f3865527
Rishav <rishav.jnit@gmail.com>
2018-09-04 23:05:49 +0530

    gnomeSort

---
+++ Sorting/gnomeSort.py
@@ -0,0 +1,12 @@
+def gnomeSort( arr, n):
+    index = 0
+    while index < n:
+        if index == 0:
+            index = index + 1
+        if arr[index] >= arr[index - 1]:
+            index = index + 1
+        else:
+            arr[index], arr[index-1] = arr[index-1], arr[index]
+            index = index - 1
+ 
+    return arr
