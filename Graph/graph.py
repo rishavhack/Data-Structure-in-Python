8f2e29dae29b186dd853c49d1688a4a60578a8f9
Rishav <rishav.jnit@gmail.com>
2019-01-14 19:28:07 +0530

    Graph

---
+++ Graph/graph.py
@@ -0,0 +1,27 @@
+class Graph: 
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [None] * self.V 
+  
+    # Function to add an edge in an undirected graph 
+    def add_edge(self, src, dest): 
+        # Adding the node to the source node 
+        node = AdjNode(dest) 
+        node.next = self.graph[src] 
+        self.graph[src] = node 
+  
+        # Adding the source node to the destination as 
+        # it is the undirected graph 
+        node = AdjNode(src) 
+        node.next = self.graph[dest] 
+        self.graph[dest] = node 
+  
+    # Function to print the graph 
+    def print_graph(self): 
+        for i in range(self.V): 
+            print("Adjacency list of vertex {}\n head".format(i), end="") 
+            temp = self.graph[i] 
+            while temp: 
+                print(" -> {}".format(temp.vertex), end="") 
+                temp = temp.next
+            print(" \n") 
